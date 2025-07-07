5ca5e84c38841e661a21baebb3d4472e715b4ff2
Adam Fuzum <atew@camelot-itlab.com>
2025-07-07 14:13:24 +0200

    test: add Cypress support files for e2e and component testing

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,3 @@
-// Custom commands for Cypress
-// For better IntelliSense support, add type definitions in cypress.d.ts
-
 // Helper function for getting elements by test ID
 function getByTestId(testId: string) {
     return cy.get(`[data-testid="${testId}"]`);
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,12 +1,3 @@
-// ***********************************************************
-// This file is used to load plugins before running your tests.
-// You can use the import statement to include your plugins.
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+
 
 export { };
