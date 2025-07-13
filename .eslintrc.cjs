9e623316ccfce2b2fc8f484ddeb5460ab90c25d7
Adam Fuzum <atew@camelot-itlab.com>
2025-07-13 15:00:38 +0200

    fix: update husky hooks and install prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-console': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  // Ignore files that you don't want ESLint to process
+  ignorePatterns: ['.commitlintrc.js'],
+};
