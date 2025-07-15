9078485900b2e86cfd92c1a10418ee85b6cadb41
Adam Fuzum <atew@camelot-itlab.com>
2025-07-15 16:04:09 +0200

    refactor(hooks): add index file for simplified imports
    Create hooks barrel file to centralize and simplify hook imports throughout the application.

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+export { default as useAuth } from './useAuth';
+export { default as useFilter } from './useFilter';
+export { default as usePagination } from './usePagination';
+export { default as usePolling } from './usePolling';
+export { default as useSearch } from './useSearch';
+export { default as useSort } from './useSort';
+export { default as useToast } from './useToast';
+export { default as useURLAnalysis } from './useURLAnalysis';
