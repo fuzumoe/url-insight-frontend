b32eb9cb8ab851819ee661e45c13a65ee818b022
Adam Fuzum <atew@camelot-itlab.com>
2025-07-15 14:17:25 +0200

    feat(layout): add index barrel file for layout components
    Create export file to simplify imports from the layout directory.

---
+++ src/components/layout/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
+export { default as Container } from './Container';
+export { default as Footer } from './Footer';
+export { default as AuthLayout } from './AuthLayout';
+export { default as MainLayout } from './MainLayout';
