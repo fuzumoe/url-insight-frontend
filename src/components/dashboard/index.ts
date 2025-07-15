13e27b8f639bf9fbc8a91fb59bfda9c857babd92
Adam Fuzum <atew@camelot-itlab.com>
2025-07-15 16:48:40 +0200

    feat(ui): add barrel files for component exports
    Create index files to simplify component imports and provide a cleaner API.

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,3 @@
+export { default as DashboardCard } from './DashboardCard';
+export { default as StatItem } from './StatItem';
+export { default as PageHeader } from './PageHeader';
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from './auth';
 export * from './common';
 export * from './layout';
 export * from './url';
+
+export * from './dashboard';
