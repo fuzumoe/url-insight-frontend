5a4c3739cdf865e7f59e67d1f1543399fb5fb92f
Adam Fuzum <atew@camelot-itlab.com>
2025-07-16 03:16:19 +0200

    refactor: reorganize components into appropriate directories
    - Move Card from dashboard to common directory
    - Move PageHeader from dashboard to layout directory
    - Move StatItem from dashboard to data directory
    - Remove redundant DashboardCard and ErrorCard in favor of configurable Card
    - Delete dashboard directory as components have been reorganized
    - Update imports and tests to reflect new component locations
    - Improve component organization based on purpose rather than usage

--- src/components/dashboard/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DashboardCard } from './DashboardCard';
-export { default as StatItem } from './StatItem';
-export { default as PageHeader } from './PageHeader';
-export { default as ErrorCard } from './ErrorCard';
